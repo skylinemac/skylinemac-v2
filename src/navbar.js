770ccfee5ced4039964080948dacecae6ccd6437
Zachary Yuan <zachary.w.yuan123@gmail.com>
2024-03-07 21:35:14 -0800

    Created Test Navbar

---
+++ src/navbar.js
@@ -0,0 +1,15 @@
+import './index.css';
+
+const Navbar = () => {
+    return (
+        <nav>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
