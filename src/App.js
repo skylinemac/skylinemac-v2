3357266c5d52daf7b7725e19c9ba2238ad84e0a0
Zachary Yuan <zachary.w.yuan123@gmail.com>
2024-03-08 10:59:06 -0800

    Button UI

--- src/App.js
+++ src/App.js
@@ -4,11 +4,8 @@ function App() {
   return (
     <div className='introButtonContainer'>
       <button onClick={() => window.location.href = '/home'} className='introButtons'>Home</button>
-      <br />
       <button onClick={() => window.location.href = '/past-tests'} className='introButtons'>Past Tests</button>
-      <br />
       <button onClick={() => window.location.href = '/competition-format'} className='introButtons'>Competition Format</button>
-      <br />
       <button onClick={() => window.location.href = '/about-us'} className='introButtons'>About Us</button>
     </div>
   );
