ba1f4398a1a476f76c004ee45a52bb8c57f22e7a
Zachary Yuan <zachary.w.yuan123@gmail.com>
2024-03-11 20:26:31 -0700

    Updated navbar for UI

--- src/navbar.js
+++ src/navbar.js
@@ -1,15 +1,24 @@
 import './index.css';
+import logo from './ClubLogo.svg';
 
 const Navbar = () => {
     return (
-        <nav className='navBar'>
-            <div className='navBarSpacing'>
+        <div className='navBar'>
+            <div className='navBarLogo'>
+                <img src={logo} alt="" width={50} height={50}/>  
+                <a href='/' className='navBarLinks' style={{
+                    marginTop: "auto",
+                    marginBottom: "auto",
+                    marginLeft: "5%"
+                }}> Skyline Mathematical Applications Contest </a>
+            </div>
+            <div className='navBarText'>
                 <a href='/home' className='navBarLinks'>Home</a>
                 <a href='/past-tests' className='navBarLinks'>Past Tests</a>
                 <a href='/competition-format' className='navBarLinks'>Competition Format</a>
                 <a href='/about-us' className='navBarLinks'>About Us</a>
             </div>
-        </nav>
+        </div>
     );
 }
 
