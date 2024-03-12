3bd8cd2b0f6f9462400bd40920e0894626c424c3
Zachary Yuan <zachary.w.yuan123@gmail.com>
2024-03-12 10:06:37 -0700

    More Navbar Changes and Home Page Editing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
+import Navbar from './navbar';
+import Picture from './IMG_7253.JPG';
 
 function App() {
   return (
-    //image for skyline logo and math club logo
-    <div className='introButtonContainer'>
-      <button onClick={() => window.location.href = '/overview'} className='introButtons'>Overview</button>
-      <button onClick={() => window.location.href = '/past-tests'} className='introButtons'>Past Tests</button>
-      <button onClick={() => window.location.href = '/competition-format'} className='introButtons'>Competition Format</button>
-      <button onClick={() => window.location.href = '/about-us'} className='introButtons'>About Us</button>
+    <div>
+      <Navbar />
+      <img src = {Picture} alt = "" height = "40%" width = "80%" />
+      <div className='introButtonContainer'>
+        <button onClick={() => window.location.href = '/overview'} className='introButtons'>Overview</button>
+        <button onClick={() => window.location.href = '/past-tests'} className='introButtons'>Past Tests</button>
+        <button onClick={() => window.location.href = '/competition-format'} className='introButtons'>Competition Format</button>
+        <button onClick={() => window.location.href = '/about-us'} className='introButtons'>About Us</button>
+      </div>
     </div>
   );
 }
